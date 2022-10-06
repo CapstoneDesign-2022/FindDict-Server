3e57fec99d6f9d9866070d1b88750e2e5c4518a4
Jimin Kim <0305jimin@gmail.com>
2022-10-06 22:00:12 +0900

    Chore: 주석 삭제#3

--- src/config/s3Config.ts
+++ src/config/s3Config.ts
@@ -1,19 +1,10 @@
 import AWS from "aws-sdk";
 import config from ".";
 
-// AWS.config.update({
-//   region: "ap-northeast-2",
-//   accessKeyId: config.awsAccessKey,
-//   secretAccessKey: config.awsSecretKey!
-// });
-
-// const s3 = new AWS.S3();
-
 const s3: AWS.S3 = new AWS.S3({
   accessKeyId: config.awsAccessKey,
   secretAccessKey: config.awsSecretKey,
   region: "ap-northeast-2"
-  //   middlewareStack:
 });
 
 export default s3;
